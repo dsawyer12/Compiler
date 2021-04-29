43a0aec6e179388c9222e03b511f1d270d1bdac2
dsawyer12 <d_sawyer12@yahoo.com>
2021-04-29 09:59:28 -0500

    finished

--- assets/assembly.asm
+++ assets/assembly.asm
@@ -22,10 +22,7 @@ section .data
 	num	times	6	db	'ABCDEF'
 	numEnd			equ	$-num
 
-	M		DW	5	
-	N		DW	3	
-	LIT20		DW	20	
-	LIT4		DW	4	
+	LIT1		DW	1	
 section	.bss
 	TempChar	RESB	1	
 	testchar	RESB	1	
@@ -33,50 +30,55 @@ section	.bss
 	tempint		RESW	1
 	negflag		RESB	1
 
-	X			RESW	1
-	Y			RESW	1
-	Z			RESW	1
+	N			RESW	1
+	FAC			RESW	1
+	I			RESW	1
+	T0			RESW	1
 	T1			RESW	1
 	T2			RESW	1
 	T3			RESW	1
+	T4			RESW	1
+	T5			RESW	1
 section	.text
 	global main
 main:
-	again: call PrintString
+
+	call PrintString
 	call GetAnInteger
 
 	mov ax, [ReadInt]
-	mov [Y], ax
+	mov [N], ax
 
-	mov ax, [ReadInt]
-	mov [Z], ax
+	mov ax, [LIT1]
+	mov [FAC], ax
 
-	mov ax, [M]
-	mul WORD [N]
+	mov ax, [LIT1]
+	mov [I], ax
+
+	L2:	nop
+	mov ax, [I]
+	cmp ax, [N]
+	jg L1
+
+	mov ax, [FAC]
+	mul WORD [I]
 	mov [T1], ax
 
 	mov ax, [T1]
-	mov [Z], ax
+	mov [FAC], ax
 
-	mov ax, [Z]
-	add ax, [Y]
-	mov [T1], ax
+	mov ax, [I]
+	add ax, [LIT1]
+	mov [T2], ax
 
-	mov ax, [T1]
-	mov [X], ax
+	mov ax, [T2]
+	mov [I], ax
 
-	mov ax, [X]
-	cmp ax, [LIT20]
-	jle L1
-
-	mov ax, [LIT4]
-	mov [X], ax
-
+	jmp	L2
 	L1:	nop
 
-	mov ax, [X]
+	mov ax, [FAC]
 	call ConvertIntegerToString
-	call PrintString
 
 	mov eax, 4
 	mov ebx, 1
